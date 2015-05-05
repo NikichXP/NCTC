ded7581199e59bc195b3604bd040f77c2204770d
Viktor Taranenko <viktor.a.taranenko@gmail.com>
2015-05-05 13:54:36 +0300

    Has to return string representations, not Objects. Updated.

--- moduleWEB/src/com/netcracker/rest/UserREST.java
+++ moduleWEB/src/com/netcracker/rest/UserREST.java
@@ -7,7 +7,6 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ws.rs.*;
 import java.math.BigInteger;
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,71 +15,71 @@ import java.util.List;
  */
 @Stateless
 @Path("user")
-public class UserREST {
-    @EJB
-    User uef;
+public class UserRest {
+	@EJB
+	User uef;
 
-    /**
-     * Login method
-     * @param name - login name to auth
-     * @param pass - password
-     * @return -
-     */
-    @GET
-    @Path ("login/{username}/{pass}")
-    @Consumes("text/plain")
-    @Produces("text/plain")
-    public String getUserID (@PathParam("username") String name, @PathParam("pass") String pass) {
-        //TODO Ask Victor how to "speak" with DB
-        List<UserEntity> user = uef.findAll();
-        String ret = "";
-        for (UserEntity anUser : user) {
-            ret += anUser.toString() + "\n";
-        }
-        return ret + name + " | " + pass; //should be changed later
+	/**
+	 * Login method
+	 * @param name - login name to auth
+	 * @param pass - password
+	 * @return -
+	 */
+	@GET
+	@Path ("login/{username}/{pass}")
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public String getUserID (@PathParam("username") String name, @PathParam("pass") String pass) {
+		//TODO Ask Victor how to "speak" with DB
+		List<UserEntity> user = uef.findAll();
+		String ret = "";
+		for (UserEntity anUser : user) {
+			ret += anUser.toString() + "\n";
+		}
+		return ret + name + " | " + pass; //should be changed later
 
-    }
+	}
 
-    @GET
-    @Path ("login/{id}")
-    @Consumes("text/plain")
-    @Produces("text/plain")
-    public String getUserByID (@PathParam("id") BigInteger id) throws Exception{
-        return uef.read(id).toString();
-    }
+	@GET
+	@Path ("login/{id}")
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public String getUserByID (@PathParam("id") BigInteger id) throws Exception{
+		return uef.read(id).toString();
+	}
 
-    @POST
-    @Path("/create/{firstName}/{lastName}/{password}")
-    @Consumes("text/plain")
-    @Produces("text/plain")
-    public String createUser(@PathParam("firstName") String firstName, @PathParam("lastName") String lastName, @PathParam("password") String pass){
-        UserEntity ue = new UserEntity();
-        ue.setFirstName(firstName);
-        ue.setLastName(lastName);
-        ue.setPassword(pass);
-        return ue.toString();
-    }
+	@POST
+	@Path("/create/{firstName}/{lastName}/{password}")
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public String createUser(@PathParam("firstName") String firstName, @PathParam("lastName") String lastName, @PathParam("password") String pass){
+		UserEntity ue = new UserEntity();
+		ue.setFirstName(firstName);
+		ue.setLastName(lastName);
+		ue.setPassword(pass);
+		return ue.toString();
+	}
 
-    @PUT
-    @Path("/update/{id}")
-    @Consumes("text/plain")
-    @Produces("text/plain")
-    public String updateByIdUser(@PathParam("id") BigInteger id){
-        UserEntity ue = uef.read(id);
-        //some to do
-        return ue.toString();
-    }
+	@PUT
+	@Path("/update/{id}")
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public String updateByIdUser(@PathParam("id") BigInteger id){
+		UserEntity ue = uef.read(id);
+		//some to do
+		return ue.toString();
+	}
 
-    @DELETE
-    @Path("/delete/{id}")
-    @Consumes("text/plain")
-    @Produces("text/plain")
-    public void deleteByIdUser(@PathParam("id") BigInteger id){
-        try {
-            uef.delete(uef.read(id));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	@DELETE
+	@Path("/delete/{id}")
+	@Consumes("text/plain")
+	@Produces("text/plain")
+	public void deleteByIdUser(@PathParam("id") BigInteger id){
+		try {
+			uef.delete(uef.read(id));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
