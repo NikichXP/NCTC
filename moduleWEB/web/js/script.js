dafb741c39c0e31a04357a6ab371f5102682b163
Viktor Taranenko <viktor.a.taranenko@gmail.com>
2015-05-10 21:50:01 +0300

    Code refactored.

--- moduleWEB/web/js/script.js
+++ moduleWEB/web/js/script.js
@@ -28,37 +28,7 @@ $('#login-submit').click(function(){
 		cred: $("#login-cred").val(),
 		pass: $("#login-pass").val()
 	};
-	$.ajax({
-		method: 'POST',
-		url: 'api/user/login',
-		contentType: "application/json; charset=utf-8",
-		data: JSON.stringify(JSONdata),
-		dataType:'text',
-		success: function (data,textStatus,jqXHR ) {
-			//alert(textStatus + " " + jqXHR.responseText);
-			document.cookie = "uuid="+jqXHR.responseText;
-			alert("uuid="+jqXHR.responseText);
-			uuid = jqXHR.responseText;
-			$.ajax({
-				method: 'POST',
-				url: 'api/user/nextAfterSubmit',
-				contentType: "text/plain; charset=utf-8",
-				data: uuid,
-				dataType:'text',
-				success: function (data,textStatus,jqXHR ) {
-					alert("User access levels: " + jqXHR.responseText);
-					document.location.href = "access_level.jsp";
-				},
-				error: function (jqXHR, textStatus, errorThrown) {
-					alert(uuid + " Error!");
-				}
-			})
-		},
-		error: function (jqXHR, textStatus, errorThrown) {
-			alert(jqXHR.status + ' ' + jqXHR.responseText);
-			//alert(eval("(" + data + ")"));
-		}
-	})
+	redirectWithAccessLevels(JSON.stringify(JSONdata), "api/user/login");
 });
 //AJAX POST for registration
 $('#registration-submit').click(function(){
@@ -69,37 +39,46 @@ $('#registration-submit').click(function(){
 		email: $("#email").val(),
 		pass: $("#reg-pass").val()
 	};
+	redirectWithAccessLevels(JSON.stringify(JSONdata), "api/user/create");
+});
+
+function redirectWithAccessLevels(userData, url) {
 	$.ajax({
 		method: 'POST',
-		url: 'api/user/create',
+		url: url,
 		contentType: "application/json; charset=utf-8",
-		data: JSON.stringify(JSONdata),
+		data: userData,
 		dataType:'text',
 		success: function (data,textStatus,jqXHR ) {
-			//alert(textStatus + ": " + jqXHR.responseText);
+			//alert(textStatus + " " + jqXHR.responseText);
 			document.cookie = "uuid="+jqXHR.responseText;
 			alert("uuid="+jqXHR.responseText);
 			uuid = jqXHR.responseText;
-			$.ajax({
-				method: 'POST',
-				url: 'api/user/nextAfterSubmit',
-				contentType: "text/plain; charset=utf-8",
-				data: uuid,
-				dataType:'text',
-				success: function (data,textStatus,jqXHR ) {
-					alert("User access levels: " + jqXHR.responseText);
-					document.location.href = "access_level.jsp";
-				},
-				error: function (jqXHR, textStatus, errorThrown) {
-					alert(uuid + " Error!");
-				}
-			})
+			getAccessLevels(uuid);
 		},
 		error: function (jqXHR, textStatus, errorThrown) {
 			alert(jqXHR.status + ' ' + jqXHR.responseText);
+			//alert(eval("(" + data + ")"));
 		}
 	})
-});
+}
+
+function getAccessLevels(uuid) {
+	$.ajax({
+		method: 'POST',
+		url: 'api/user/nextAfterSubmit',
+		contentType: "text/plain; charset=utf-8",
+		data: uuid,
+		dataType:'text',
+		success: function (data,textStatus,jqXHR ) {
+			alert("User access levels: " + jqXHR.responseText);
+			document.location.href = "access_level.jsp";
+		},
+		error: function (jqXHR, textStatus, errorThrown) {
+			alert(uuid + " Error!");
+		}
+	})
+}
 //AJAX POST for
 $('#track-TO-submit').click(function(){
 	JSONdata = {
