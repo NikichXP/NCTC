2104bbc0ebb1512d916b3827184f9ad766bed893
Roman Kulyk <soldierzack1@gmail.com>
2015-05-14 20:25:02 +0300

    set admin dashboard to program logic

---
+++ moduleWEB/web/js/driver-script.js
@@ -0,0 +1,31 @@
+//Auxiliary counter to add number to fresh OT divs
+var counter = 0;
+//Creating Order Blocks (in form of div in html code) by pressing MAKE ORDER button.
+//Do not delete! This function will be required later
+$("#centralButton").click(function () {
+	var newOrderPlate = document.createElement('div');
+	newOrderPlate.className = 'orderPlate';
+	newOrderPlate.id = 'taxiOrder'  + counter++;
+	newOrderPlate.textContent = 'TAXI ORDER # ' + counter;
+	applyStyle (newOrderPlate);
+	document.getElementById('customerOrdersPanel').appendChild(newOrderPlate);
+});
+//Apply style to dynamically creater divs (Order Blocks).
+//TO BE COMPLETED. Style constant must be developed
+function applyStyle (elementToStyle){
+	elementToStyle.style.height = '150px';
+	elementToStyle.style.width = '95%';
+	elementToStyle.style.border = 'solid black';
+	elementToStyle.style.marginTop = '2px';
+	elementToStyle.style.marginBottom = '2px';
+	elementToStyle.style.marginLeft = 'auto';
+	elementToStyle.style.marginRight = 'auto';
+	elementToStyle.style.backgroundColor = '#6AD644';
+}
+//Remove Order by clicking it
+//Just for the showcase
+$(".mainSection").on('click', 'div.orderPlate', function(event) {
+    alert('you clicked an OrderBlock element, now it will be removed. Blame yourself...');
+    $(this).remove();
+});
+
