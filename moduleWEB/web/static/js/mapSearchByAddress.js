26e3b4f549ecd1c10b861d3ee2d942e732b7abe8
atsvirchkova <atsvirchkova@gmail.com>
2015-05-14 13:26:54 +0300

    Path between from and to on map
    Path between from and to on map

--- moduleWEB/web/static/js/mapSearchByAddress.js
+++ moduleWEB/web/static/js/mapSearchByAddress.js
@@ -1,8 +1,8 @@
 /**
  * Created by Ann on 09.05.2015.
  */
-var geoResult;
 var myMap;
+var myPath;
 //var rootURL = "http://localhost:8085/TestMaps_war_exploded/map.html";
 //var coord;
 
@@ -22,9 +22,9 @@ function init () {
         zoom: 10
     });
 }
+
 function makeSearch(value, resultx,resulty){
    // var value = document.getElementById("fromAddress0").value;
-    console.log(value);
     ymaps.geocode(value, {
         /**
          * Опции запроса
@@ -49,35 +49,31 @@ function makeSearch(value, resultx,resulty){
         myMap.setBounds(bounds, {
             checkZoomRange: true // проверяем наличие тайлов на данном масштабе.
         });
-
-        /**
-         * Все данные в виде javascript-объекта.
-         */
-        console.log("er Все данные геообъекта: ", firstGeoObject.properties.getAll());
-        /**
-         * Метаданные запроса и ответа геокодера.
-         * @see https://api.yandex.ru/maps/doc/geocoder/desc/reference/GeocoderResponseMetaData.xml
-         */
-        console.log('Метаданные ответа геокодера: ', res.metaData);
-        /**
-         * Метаданные геокодера, возвращаемые для найденного объекта.
-         * @see https://api.yandex.ru/maps/doc/geocoder/desc/reference/GeocoderMetaData.xml
-         */
-        console.log('Метаданные геокодера: ', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData'));
-        /**
-         * Точность ответа (precision) возвращается только для домов.
-         * @see https://api.yandex.ru/maps/doc/geocoder/desc/reference/precision.xml
-         */
-        console.log('precision', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData.precision'));
-        /**
-         * Тип найденного объекта (kind).
-         * @see https://api.yandex.ru/maps/doc/geocoder/desc/reference/kind.xml
-         */
-        console.log('Тип геообъекта: %s', firstGeoObject.properties.get('metaDataProperty.GeocoderMetaData.kind'));
-        console.log('Название объекта: %s', firstGeoObject.properties.get('name'));
-        console.log('Описание объекта: %s', firstGeoObject.properties.get('description'));
-        console.log('Полное описание объекта: %s', firstGeoObject.properties.get('text'));
     });
+    if (document.getElementById('fromAddress').value!='' && document.getElementById('toAddress').value!=''){
+        buildPath();
+    }
     return false;
 }
 
+function buildPath(){
+    if (myPath!=undefined){
+        myMap.geoObjects.remove(myPath);
+    }
+    var fromPoint=document.getElementById('fromAddress').value;
+    var toPoint=document.getElementById('toAddress').value;
+    myPath= new ymaps.route([
+        fromPoint,
+        toPoint
+    ], {
+        // Router options
+        mapStateAutoApply: true // automatically position the map
+    }).then(function (route) {
+        myMap.geoObjects.add(route);
+        document.getElementById('totalLength').value=route.getLength();
+    }, function (error) {
+        alert("An error occurred: " + error.message);
+    });
+
+    return false;
+}
