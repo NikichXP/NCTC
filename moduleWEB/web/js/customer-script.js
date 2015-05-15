5d177501406ae2802006ad2e846ebc6f201fe661
JugerMaximus <JugerMaximus@gmail.com>
2015-05-15 22:13:17 +0300

    back customer js

--- moduleWEB/web/js/customer-script.js
+++ moduleWEB/web/js/customer-script.js
@@ -1,158 +1,37 @@
-//Actual logic
-
-// On page load {
-// 	get data from server (ajax call)
-// 	build Order Blocks from received data
-// 	add them to HTML
-// }
-// How to delete blocks {
-// 	listen to server
-// 	delete specified blocks
-// }
-var obj = [
-{
-	"Status": "assigned",
-	"Type": "regular ride",
-	"From": "obolon",
-	"To": "kpi",
-	"Time": "14:08"
-},
-{
-	"Status": "assigned",
-	"Type": "regular ride",
-	"From": "obolon",
-	"To": "kpi",
-	"Time": "14:08"
-},
-{
-	"Status": "assigned",
-	"Type": "regular ride",
-	"From": "obolon",
-	"To": "kpi",
-	"Time": "14:08"
-},
-{
-	"Status": "assigned",
-	"Type": "regular ride",
-	"From": "obolon",
-	"To": "kpi",
-	"Time": "14:08"
-}
-];
-
-$(function(){
-
-	//Actual logic. Uncomment it
-
-	// $.ajax({
-	// 	method: 'POST',
-	// 	url: 'api/user_dash/history',
-	// 	contentType: "text/plain; charset=utf-8",
-	// 	data: uuid,
-	// 	dataType: 'text',
-	// 	success: function (data, textStatus, jqXHR) {
-	// 		var obj = JSON.parse(data);
-	// 		alert(data);
-
-	 		//Iterates through received array (obj) to build html elements based on EACH order JSON
-	// 		for(var order in obj){
-	//		createOrderPlate(obj[order]);
-	// 		}
-	// 	},
-	// 	error: function (jqXHR, textStatus, errorThrown) {
-	// 		alert(jqXHR.responseText + " Error!");
-	// 	}
-	// })
-
-	
-
-	//For the purpose of debugging. DELETE IT.
-	for(var order in obj){
-		createOrderPlate(obj[order]);
-	}
-
-	
-});
-
-function constructOrderText (order) {
-
-	var orderText = document.createElement('div');
-	//Extracting data from JSON to construct HTML
-	var orderStatus = 'Status:' + order["Status"];
-	var orderType = 'Type ' + order["Type"];
-	var orderFrom = 'From: ' + order["From"];
-	var orderTo = 'To: ' + order["To"];
-	var orderTime = 'Time: ' + order["Time"];
-	var orderTextHTML = '<div><h3>description:</h3><p>' + orderStatus + '</p><p>' + orderType + '</p><p>' + orderFrom + '</p><p>' + orderTo + '</p><p>' + orderTime + '</p></div>';
-	
-	//Putting HTML with data inside order text block
-	orderText.innerHTML = orderTextHTML;
-	//Applying style to 
-	orderText.style.fontWeight = 'bold';
-	orderText.style.float = 'left';
-	orderText.style.backgroundColor = 'red';
-
-	return orderText;
-}
-
-function constructOrderIcon (order) {
-
-	
-	var orderIcon = document.createElement('div');
-
-	orderIcon.style.float = 'right';
-	orderIcon.style.backgroundColor = 'green';
-
-
-
-	var animal = 'Animal' + order["Animal"];
-	var orderIconHTML = '<div><h3>description:</h3><p>' + animal + '</p><p></div>';
-
-
-
-	orderIcon.innerHTML = orderIconHTML;
-
-	return orderIcon;
-}
-
-function createOrderPlate (order) {
-
-	//creating order plate which will contain information about order. 
-	var newOrderPlate = document.createElement('div');
-	//Setting id for newly created plate. By clicking plate user will be
-	//redirected to the OrderPage with corresponded id.
-
-	//styling
-	newOrderPlate.style.height = 'auto';
-	newOrderPlate.style.width = '95%';
-	newOrderPlate.style.border = 'solid black';
-	newOrderPlate.style.marginTop = '2px';
-	newOrderPlate.style.marginBottom = '2px';
-	newOrderPlate.style.marginLeft = 'auto';
-	newOrderPlate.style.marginRight = 'auto';
-	newOrderPlate.style.backgroundColor = '#53C68A';
-
-
-
-	var text = constructOrderText(order);
-
-	var icon = constructOrderIcon(order);
-	//first attach parents element to the DOM
-	document.getElementById('customerOrdersPanel').appendChild(newOrderPlate);
-	//then attach children to the parent
-
-	newOrderPlate.appendChild(text);
-	newOrderPlate.appendChild(icon);	
-
-}
-
-
+//Auxiliary counter to add number to fresh OT divs
+var counter = 0;
 //Creating Order Blocks (in form of div in html code) by pressing MAKE ORDER button.
 //Do not delete! This function will be required later
+$("#centralButton").click(function () {
+	document.location.href = "createOrder.jsp";
+	//var newOrderPlate = document.createElement('div');
+	//newOrderPlate.className = 'orderPlate';
+	//newOrderPlate.id = 'taxiOrder'  + counter++;
+	//newOrderPlate.textContent = 'TAXI ORDER # ' + counter;
+	//applyStyle (newOrderPlate);
+	//document.getElementById('customerOrdersPanel').appendChild(newOrderPlate);
+});
 
-$("#centralButton").click(function () {	
-	//window.location = "createOrder.jsp";
+$("#leftButton").click(function () {
+	document.location.href = "customerOrderHistory.jsp";
 });
 
 
-
+//Apply style to dynamically creater divs (Order Blocks).
+//TO BE COMPLETED. Style constant must be developed
+function applyStyle (elementToStyle){
+	elementToStyle.style.height = '150px';
+	elementToStyle.style.width = '95%';
+	elementToStyle.style.border = 'solid black';
+	elementToStyle.style.marginTop = '2px';
+	elementToStyle.style.marginBottom = '2px';
+	elementToStyle.style.marginLeft = 'auto';
+	elementToStyle.style.marginRight = 'auto';
+	elementToStyle.style.backgroundColor = '#6AD644';
+}
+//Remove Order by clicking it
+//Just for the showcase
+$(".mainSection").on('click', 'div.orderPlate', function(event) {
+	alert('you clicked an OrderBlock element, now it will be removed. Blame yourself...');
+	$(this).remove();
+});
