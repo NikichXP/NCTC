c95dfba2602b8c7327c7de0a26eea502ed8fd82a
Alexandelphi <alexandelphi@gmail.com>
2015-05-19 19:13:29 +0300

    DriverRest + JS

---
+++ moduleWEB/web/js/driver_orders.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    getOrders();
+});
+
+function getOrders(){
+        $.ajax({
+            method: 'POST',
+            url: 'api/driver/getOrders',
+            contentType: "text/plain; charset=utf-8",
+            dataType: 'text',
+            success: function (data, textStatus, jqXHR) {
+                var obj = JSON.parse(data);
+                alert(obj);
+                drawTable(obj.orders, "driverQueuedOrdersPanel");
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(jqXHR.responseText + " Error!");
+            }
+        })
+
+}
+
+
+function drawTable(data, table) {
+    for (var i = 0; i < data.length; i++) {
+        drawBody(data[i], table);
+    }
+}
+function drawBody(rowData, table) {
+
+    var div = document.getElementById(table);
+    var createDiv = document.createElement("div");
+    var node = document.createTextNode("date :" + rowData.dateOrderCreate
+    + " first point :" + rowData.startOrder
+    + " next point :" + rowData.endOrder
+    + " status :" + rowData.statusOrder
+    + " price: " + rowData.price + "$");
+    createDiv.appendChild(node);
+    createDiv.className = "button";
+    createDiv.onclick = function () {
+        document.location.href = "editOrder.jsp?id=" + rowData.id;
+    };
+    div.appendChild(createDiv);
+}
