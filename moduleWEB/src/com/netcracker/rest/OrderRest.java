29c7c9af00d0c16c031e2fcd9d3c336e557de6fe
Viktor Taranenko <viktor.a.taranenko@gmail.com>
2015-05-20 12:56:58 +0300

    Order view @GET method added.

--- moduleWEB/src/com/netcracker/rest/OrderRest.java
+++ moduleWEB/src/com/netcracker/rest/OrderRest.java
@@ -6,9 +6,7 @@ import com.netcracker.entity.PathEntity;
 import com.netcracker.facade.local_int.*;
 
 import javax.ejb.EJB;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -21,7 +19,7 @@ import java.util.*;
  * Created by Juger on 05.05.2015.
  */
 
-@Path("order")
+@javax.ws.rs.Path("order")
 public class OrderRest {
 	@EJB
 	private User user;
@@ -38,8 +36,10 @@ public class OrderRest {
 	@EJB
 	private com.netcracker.facade.local_int.Path path;
 
+	private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+
 	@POST
-	@Path("create")
+	@javax.ws.rs.Path("create")
 	@Consumes("application/json")
 	public Response createOrder(OrderJson orderJson) {
 		OrderEntity orderEntity = new OrderEntity();
@@ -59,7 +59,7 @@ public class OrderRest {
 		if (!orderJson.isAsSoonAsPossible()) {
 			try {
 				orderEntity.setTimeRequested(new Timestamp(
-						new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(orderJson.getTimeRequested()
+						simpleDateFormat.parse(orderJson.getTimeRequested()
 						).getTime()));
 			} catch (ParseException e) {
 				e.printStackTrace();
@@ -132,13 +132,100 @@ public class OrderRest {
 		} else {
 			return Response.status(201).entity(orderJson.toString() + "Order id: " + orderEntity.getId()).build();
 		}
+	}
 
+	@GET
+	@javax.ws.rs.Path("view")
+	public Response viewOrder(@QueryParam("id") String orderId) {
+		OrderEntity orderEntity = order.read(new BigInteger(orderId));
+		OrderJson orderJson = new OrderJson();
+
+		orderJson.setId(orderEntity.getId().toString());
+		orderJson.setContactName(orderEntity.getContactName());
+		orderJson.setContactPhone(orderEntity.getContactPhone());
+
+		orderJson.setType(orderEntity.getOrderTypeEntity().getName());
+		orderJson.setState(orderEntity.getOrderStateEntity().getName());
+
+		orderJson.setTimeCreated(
+				simpleDateFormat.format(
+						new Date(orderEntity.getTimeCreated().getTime())));
+		orderJson.setTimeRequested(
+				simpleDateFormat.format(
+						new Date(orderEntity.getTimeRequested().getTime())));
+		if (orderEntity.getTimeOfDriverArrival() != null) {
+			orderJson.setTimeOfDriverArrival(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeOfDriverArrival().getTime())));
+		}
+		if (orderEntity.getTimeStarted() != null) {
+			orderJson.setTimeStarted(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeStarted().getTime())));
+		}
+		if (orderEntity.getTimeCompleted() != null) {
+			orderJson.setTimeCompleted(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeCompleted().getTime())));
+		}
+
+		List<PathEntity> pathEntities = new LinkedList<>(orderEntity.getPathEntities());
+		pathEntities.sort(new Comparator<PathEntity>() {
+			@Override
+			public int compare(PathEntity o1, PathEntity o2) {
+				return o1.getId().compareTo(o2.getId());
+			}
+		});
+		orderJson.setFromAddress(pathEntities.get(0).getStartAddress());
+		orderJson.setFromX(pathEntities.get(0).getStartX().toString());
+		orderJson.setFromY(pathEntities.get(0).getStartY().toString());
+
+		List<String> endAddresses = new LinkedList<>();
+		List<String> endXs = new LinkedList<>();
+		List<String> endYs = new LinkedList<>();
+		List<String> distances = new LinkedList<>();
+
+		for (PathEntity pathEntity : pathEntities) {
+			endAddresses.add(pathEntity.getEndAddress());
+			endXs.add(pathEntity.getEndX().toString());
+			endYs.add(pathEntity.getEndY().toString());
+			distances.add(pathEntity.getLength().toString());
+		}
+
+		orderJson.setSex(orderEntity.getDriverSex());
+		orderJson.setCarClass(orderEntity.getCarClassEntity().getName());
+		orderJson.setMusicType(orderEntity.getMusicTypeEntity().getName());
+		orderJson.setSmokingFriendly(orderEntity.getSmokingFriendly());
+		orderJson.setAnimalFriendly(orderEntity.getAnimalFriendly());
+		orderJson.setWifi(orderEntity.getWifi());
+		orderJson.setAirConditioner(orderEntity.getAirConditioner());
+
+		orderJson.setCustomerPreCreateComment(orderEntity.getCustomerPreCreateComment());
+		orderJson.setCustomerPostCompleteComment(orderEntity.getCustomerPostCompleteComment());
+		if(orderEntity.getRefuseCauseByCustomerEntity() != null)
+			orderJson.setCustomerRefuseCause(orderEntity.getRefuseCauseByCustomerEntity().getMessage());
+		if(orderEntity.getRefuseCauseByDriverEntity() != null)
+		orderJson.setDriverRefuseCause(orderEntity.getRefuseCauseByDriverEntity().getMessage());
+		orderJson.setCustomerRefuseComment(orderEntity.getCustomerRefuseComment());
+		orderJson.setDriverRefuseComment(orderEntity.getDriverRefuseComment());
+
+		if (orderEntity.getTotalLength() != null) {
+			orderJson.setTotalLength(orderEntity.getTotalLength().toString());
+			orderJson.setTotalMultiplier(orderEntity.getTotalMultiplier().toString());
+			orderJson.setTotalPrice(orderEntity.getTotalMultiplier().multiply(orderEntity.getTotalLength()).toString());
+		}
+
+		if (orderJson == null) {
+			return Response.status(404).entity("OrderJson is null.").build();
+		} else {
+			return Response.status(201).entity(orderJson.toString()).build();
+		}
 	}
 
 	@POST
-	@Path("edite")
+	@javax.ws.rs.Path("edit")
 	@Consumes("application/json")
-	public Response editOrder(OrderJson orderJson){
+	public Response editOrder(OrderJson orderJson) {
 		OrderEntity orderEntity = order.read(new BigInteger(orderJson.getId()));
 		orderEntity.setCustomerUserEntity(user.findByUuid(orderJson.getCustomerUserUuid()));
 		orderEntity.setPublicToken(UUID.randomUUID().toString().substring(0, 8));
@@ -155,7 +242,7 @@ public class OrderRest {
 		if (!orderJson.isAsSoonAsPossible()) {
 			try {
 				orderEntity.setTimeRequested(new Timestamp(
-						new SimpleDateFormat("dd/MM/yyyy hh:mm").parse(orderJson.getTimeRequested()
+						simpleDateFormat.parse(orderJson.getTimeRequested()
 						).getTime()));
 			} catch (ParseException e) {
 				e.printStackTrace();
@@ -165,7 +252,7 @@ public class OrderRest {
 		}
 
 		List<PathEntity> pathEntities = new ArrayList<>(orderEntity.getPathEntities());
-		for (PathEntity pathE: pathEntities){
+		for (PathEntity pathE : pathEntities) {
 			path.delete(pathE);
 		}
 
