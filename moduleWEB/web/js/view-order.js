4319ead7e1d87168ecb31e4ce8bfeafe2876f84b
Alexandelphi <alexandelphi@gmail.com>
2015-05-22 03:55:15 +0300

    some fix

--- moduleWEB/web/js/view-order.js
+++ moduleWEB/web/js/view-order.js
@@ -15,14 +15,46 @@ function getOrderById(id) {
         $("#fromX").attr("value", obj.fromX);
         $("#fromX").attr("value", obj.fromY);
 
+        //TODO add dynamic generation
         $("#toAddress0").attr("value", obj.toAddress[0]);
+        $("#toX0").attr("value", obj.toX[0]);
+        $("#toY0").attr("value", obj.toY[0]);
+        $("#distance0").attr("value", obj.distance[0]);
 
+        $("#timeRequested").attr("value", obj.timeRequested);
+
+        $('input:radio[name=sex][data-value=' + obj.sex + ']').prop('checked', true);
+
+        //TODO override getCarClass() method dynamic generation in createorder.js : carClass = disabled. Make copy file.
+        $('input:radio[name=carClass][data-value=' + obj.carClass + ']').prop('checked', true);
+
+        $("#music").attr("value", obj.musicType);
+
+        if(obj.smokingFriendly == "true") {
+            $('input:checkbox[id=smokingFriendly]').prop('checked', true);
+        }
+        if(obj.animalFriendly == "true") {
+            $('input:checkbox[id=animalFriendly]').prop('checked', true);
+        }
+        if(obj.wifi == "true") {
+            $('input:checkbox[id=wifi]').prop('checked', true);
+        }
+        if(obj.airConditioner == "true") {
+            $('input:checkbox[id=airConditioner]').prop('checked', true);
+        }
+
+        //TODO this value is empty
+        $("#totalMultiplier").attr("value", obj.totalMultiplier);
+        $("#totalLength").attr("value", obj.totalLength);
+        $("#totalPrice").attr("value", obj.totalPrice);
+
+        $("#customerPreCreateComment").attr("value", obj.customerPreCreateComment);
     });
 }
 
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
