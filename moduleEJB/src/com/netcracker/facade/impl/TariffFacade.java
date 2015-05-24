d060b785d401f28680669fed4c47661763033ebb
Baggiopanics <baggiopanics@gmail.com>
2015-05-24 19:05:23 +0300

    TariffFacades now suits update requirements.

--- moduleEJB/src/com/netcracker/facade/impl/TariffFacade.java
+++ moduleEJB/src/com/netcracker/facade/impl/TariffFacade.java
@@ -2,12 +2,15 @@ package com.netcracker.facade.impl;
 
  /* 18:42 28.04.2015 by Viktor Taranenko */
 
+import com.netcracker.entity.CarClassEntity;
+import com.netcracker.entity.OrderTypeEntity;
 import com.netcracker.entity.TariffEntity;
 import com.netcracker.facade.local_int.Tariff;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.math.BigDecimal;
 
 @Stateless
 public class TariffFacade extends AbstractFacade<TariffEntity> implements Tariff {
@@ -22,5 +25,77 @@ public class TariffFacade extends AbstractFacade<TariffEntity> implements Tariff
     public TariffFacade() {
         super(TariffEntity.class);
     }
-    
+
+    @Override
+    public void setDayHourlyMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setDayHourlyMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+
+    }
+
+    @Override
+    public void setNightHourlyMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setNightHourlyMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+
+    }
+
+    @Override
+    public void setDayDistanceMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setDayDistanceMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+
+    }
+
+    @Override
+    public void setNightDistanceMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setNightDistanceMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+    }
+
+    @Override
+    public void setSmokingFriendlyMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setSmokingFriendlyMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+    }
+
+    @Override
+    public void setAnimalFriendlyMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setAnimalFriendlyMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+    }
+
+    @Override
+    public void setAirConditionerMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setAirConditionerMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+    }
+
+    @Override
+    public void setWifiMultiplier(BigDecimal multiplier) {
+        em.createNamedQuery("Tariff.setWifiMultiplier")
+                .setParameter("multiplier", multiplier).executeUpdate();
+    }
+
+    @Override
+    public void setSexMultiplier(BigDecimal multiplier, String sex) {
+        em.createNamedQuery("Tariff.setSexMultiplier")
+                .setParameter("multiplier", multiplier)
+                .setParameter("sex", sex).executeUpdate();
+    }
+
+    @Override
+    public void setCarClassMultiplier(BigDecimal multiplier, CarClassEntity carClassEntity) {
+        em.createNamedQuery("Tariff.setCarClassMultiplier")
+                .setParameter("multiplier", multiplier)
+                .setParameter("carClassEntity", carClassEntity).executeUpdate();
+    }
+
+    @Override
+    public void setOrderTypeMultiplier(BigDecimal multiplier, OrderTypeEntity orderTypeEntity) {
+        em.createNamedQuery("Tariff.setOrderTypeMultiplier")
+                .setParameter("multiplier", multiplier)
+                .setParameter("orderTypeEntity", orderTypeEntity).executeUpdate();
+    }
+
 }
--- moduleEJB/src/com/netcracker/facade/local_int/Tariff.java
+++ moduleEJB/src/com/netcracker/facade/local_int/Tariff.java
@@ -2,9 +2,12 @@ package com.netcracker.facade.local_int;
 
 /* 0:25 04.05.2015 by Viktor Taranenko */
 
+import com.netcracker.entity.CarClassEntity;
+import com.netcracker.entity.OrderTypeEntity;
 import com.netcracker.entity.TariffEntity;
 
 import javax.ejb.Local;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Local
@@ -22,4 +25,26 @@ public interface Tariff {
 	List<TariffEntity> findRange(int[] range);
 
 	int count();
+
+	void setDayHourlyMultiplier(BigDecimal multiplier);
+
+	void setNightHourlyMultiplier(BigDecimal multiplier);
+
+	void setDayDistanceMultiplier(BigDecimal multiplier);
+
+	void setNightDistanceMultiplier(BigDecimal multiplier);
+
+	void setSmokingFriendlyMultiplier(BigDecimal multiplier);
+
+	void setAnimalFriendlyMultiplier(BigDecimal multiplier);
+
+	void setAirConditionerMultiplier(BigDecimal multiplier);
+
+	void setWifiMultiplier(BigDecimal multiplier);
+
+	void setSexMultiplier(BigDecimal multiplier, String sex);
+
+	void setCarClassMultiplier(BigDecimal multiplier, CarClassEntity carClassEntity);
+
+	void setOrderTypeMultiplier(BigDecimal multiplier, OrderTypeEntity orderTypeEntity);
 }
