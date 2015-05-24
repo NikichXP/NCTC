032c716e548607811f58ea36e683a379e301e0d2
Alexandelphi <alexandelphi@gmail.com>
2015-05-25 00:33:24 +0300

    add view queued order JS and etc. + some fix

--- moduleWEB/src/com/netcracker/rest/OrderRest.java
+++ moduleWEB/src/com/netcracker/rest/OrderRest.java
@@ -265,6 +265,125 @@ public class OrderRest {
 		return Response.status(201).entity(orderJson.toString()).build();
 	}
 
+	@GET
+	@javax.ws.rs.Path("viewQueuedOrder")
+	public Response viewQueuedOrders(@QueryParam("id") String orderId) {
+		OrderEntity orderEntity = order.read(new BigInteger(orderId));
+
+		if (orderEntity == null) {
+			return Response.status(404).entity("No such order in DB.").build();
+		}
+
+		OrderJson orderJson = new OrderJson();
+
+		orderJson.setId(orderEntity.getId().toString());
+		orderJson.setContactName(orderEntity.getContactName());
+		orderJson.setContactPhone(orderEntity.getContactPhone());
+		orderJson.setRequestedSeatsCount(orderEntity.getRequestedSeatsCount().toString());
+
+		orderJson.setType(orderEntity.getOrderTypeEntity().getName());
+		orderJson.setState(orderEntity.getOrderStateEntity().getName());
+
+		orderJson.setPublicToken(orderEntity.getPublicToken());
+		orderJson.setCustomerUserId(orderEntity.getCustomerUserEntity().getId().toString());
+		orderJson.setCustomerUserUuid(orderEntity.getCustomerUserEntity().getUuid());
+		//orderJson.setDriverUserId(orderEntity.getDriverUserEntity().getId().toString());
+
+		orderJson.setTimeCreated(
+				simpleDateFormat.format(
+						new Date(orderEntity.getTimeCreated().getTime())));
+		orderJson.setTimeRequested(
+				simpleDateFormat.format(
+						new Date(orderEntity.getTimeRequested().getTime())));
+		if (orderEntity.getTimeOfDriverArrival() != null) {
+			orderJson.setTimeOfDriverArrival(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeOfDriverArrival().getTime())));
+		}
+		if (orderEntity.getTimeStarted() != null) {
+			orderJson.setTimeStarted(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeStarted().getTime())));
+		}
+		if (orderEntity.getTimeCompleted() != null) {
+			orderJson.setTimeCompleted(
+					simpleDateFormat.format(
+							new Date(orderEntity.getTimeCompleted().getTime())));
+		}
+
+		List<PathEntity> pathEntities = new LinkedList<>(orderEntity.getPathEntities());
+		pathEntities.sort(new Comparator<PathEntity>() {
+			@Override
+			public int compare(PathEntity o1, PathEntity o2) {
+				return o1.getId().compareTo(o2.getId());
+			}
+		});
+		orderJson.setFromAddress(pathEntities.get(0).getStartAddress());
+		orderJson.setFromX(pathEntities.get(0).getStartX().toString());
+		orderJson.setFromY(pathEntities.get(0).getStartY().toString());
+
+		List<String> endAddresses = new LinkedList<>();
+		List<String> endXs = new LinkedList<>();
+		List<String> endYs = new LinkedList<>();
+		List<String> distances = new LinkedList<>();
+		List<String> pathIds = new LinkedList<>();
+		List<String> isPathCompleted = new LinkedList<>();
+
+		for (PathEntity pathEntity : pathEntities) {
+			endAddresses.add(pathEntity.getEndAddress());
+			endXs.add(pathEntity.getEndX().toString());
+			endYs.add(pathEntity.getEndY().toString());
+			distances.add(pathEntity.getLength().toString());
+			pathIds.add(pathEntity.getId().toString());
+			isPathCompleted.add(String.valueOf(pathEntity.isCompleted()));
+		}
+
+		orderJson.setToAddress(endAddresses.toArray(new String[endAddresses.size()]));
+		orderJson.setToX(endXs.toArray(new String[endXs.size()]));
+		orderJson.setToY(endYs.toArray(new String[endYs.size()]));
+		orderJson.setDistance(distances.toArray(new String[distances.size()]));
+		orderJson.setPathId(pathIds.toArray(new String[pathIds.size()]));
+		orderJson.setPathId(pathIds.toArray(new String[pathIds.size()]));
+		orderJson.setPathCompleted(isPathCompleted.toArray(new String[isPathCompleted.size()]));
+
+		orderJson.setSex(orderEntity.getDriverSex());
+		orderJson.setCarClass(orderEntity.getCarClassEntity().getName());
+		orderJson.setMusicType(orderEntity.getMusicTypeEntity().getName());
+		orderJson.setSmokingFriendly(orderEntity.getSmokingFriendly());
+		orderJson.setAnimalFriendly(orderEntity.getAnimalFriendly());
+		orderJson.setWifi(orderEntity.getWifi());
+		orderJson.setAirConditioner(orderEntity.getAirConditioner());
+
+		orderJson.setCustomerPreCreateComment(orderEntity.getCustomerPreCreateComment());
+		if(orderEntity.getCustomerPostCompleteComment() != null) {
+			orderJson.setCustomerPostCompleteComment(orderEntity.getCustomerPostCompleteComment());
+		} else orderJson.setCustomerPostCompleteComment("");
+		if(orderEntity.getRefuseCauseByCustomerEntity() != null) {
+			orderJson.setCustomerRefuseCause(orderEntity.getRefuseCauseByCustomerEntity().getMessage());
+		} else orderJson.setCustomerRefuseCause("");
+		if(orderEntity.getRefuseCauseByDriverEntity() != null) {
+			orderJson.setDriverRefuseCause(orderEntity.getRefuseCauseByDriverEntity().getMessage());
+		} else orderJson.setDriverRefuseCause("");
+		if(orderEntity.getCustomerRefuseComment() != null) {
+			orderJson.setCustomerRefuseComment(orderEntity.getCustomerRefuseComment());
+		} else orderJson.setCustomerRefuseComment("");
+		if(orderEntity.getDriverRefuseComment() != null) {
+			orderJson.setDriverRefuseComment(orderEntity.getDriverRefuseComment());
+		} else orderJson.setDriverRefuseComment("");
+
+		if(orderEntity.getTotalLength() != null && orderEntity.getTotalMultiplier() != null) {
+			orderJson.setTotalLength(orderEntity.getTotalLength().toString());
+			orderJson.setTotalMultiplier(orderEntity.getTotalMultiplier().toString());
+			orderJson.setTotalPrice(orderEntity.getTotalMultiplier().multiply(orderEntity.getTotalLength()).toString());
+		} else {
+			orderJson.setTotalLength("");
+			orderJson.setTotalMultiplier("");
+			orderJson.setTotalPrice("");
+		}
+
+		return Response.status(201).entity(orderJson.toString()).build();
+	}
+
 	@POST
 	@javax.ws.rs.Path("edit")
 	@Consumes("application/json")
---
+++ moduleWEB/web/js/view-queued-order.js
@@ -0,0 +1,61 @@
+/**
+ * Created by Ubuntu on 20.05.2015.
+ */
+$(document).ready(function () {
+    getUrlVars();
+    getOrderById(getUrlVars()["id"]);
+    ymaps.ready(init);
+});
+
+function getOrderById(id) {
+    $.get("api/order/viewQueuedOrder?id=" + id, function (data) {
+        var obj = JSON.parse(data);
+        $("#contactName").attr("value", obj.contactName);
+        $("#contactPhone").attr("value", obj.contactPhone);
+        $("#requestedSeatsCount").attr("value", obj.requestedSeatsCount);
+        $("#fromAddress").attr("value", obj.fromAddress);
+        $("#fromX").attr("value", obj.fromX);
+        $("#fromY").attr("value", obj.fromY);
+
+        //TODO add dynamic generation
+        $("#toAddress0").attr("value", obj.toAddress[0]);
+        $("#toX0").attr("value", obj.toX[0]);
+        $("#toY0").attr("value", obj.toY[0]);
+        $("#distance0").attr("value", obj.distance[0]);
+
+        $("#timeRequested").attr("value", obj.timeRequested);
+
+        $('input:radio[name=sex][data-value=' + obj.sex + ']').prop('checked', true);
+
+        $("#car").attr("value", obj.carClass);
+        $("#music").attr("value", obj.musicType);
+
+        if(obj.smokingFriendly == "true") {
+            $('input:checkbox[id=smokingFriendly]').prop('checked', true);
+        }
+        if(obj.animalFriendly == "true") {
+            $('input:checkbox[id=animalFriendly]').prop('checked', true);
+        }
+        if(obj.wifi == "true") {
+            $('input:checkbox[id=wifi]').prop('checked', true);
+        }
+        if(obj.airConditioner == "true") {
+            $('input:checkbox[id=airConditioner]').prop('checked', true);
+        }
+
+        //TODO this value is empty
+        $("#totalMultiplier").attr("value", obj.totalMultiplier);
+        $("#totalLength").attr("value", obj.totalLength);
+        $("#totalPrice").attr("value", obj.totalPrice);
+
+        $("#customerPreCreateComment").attr("value", obj.customerPreCreateComment);
+    });
+}
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+        vars[key] = value;
+    });
+    return vars;
+}
