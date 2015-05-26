fe3a2131dbb39d616849e652d0dcc3ad0c593f99
Alexandelphi <alexandelphi@gmail.com>
2015-05-26 18:21:45 +0300

    add queued -> assigned

--- moduleWEB/web/js/view-queued-order.js
+++ moduleWEB/web/js/view-queued-order.js
@@ -2,14 +2,44 @@
  * Created by Ubuntu on 20.05.2015.
  */
 $(document).ready(function () {
+    $(function ($) {
+        $("#timeOfDriverArrival").mask("99/99/9999 99:99", {placeholder: "dd/mm/yyyy hh:mm"});
+    });
     getUrlVars();
     getOrderById(getUrlVars()["id"]);
     ymaps.ready(init);
+
+    $("#assignButton").click(function () {
+        $("#assignButton").prop('disabled', true);
+        var JSONdata = {
+            id: obj.id,
+            driverUserUuid: getCookie("uuid"),
+            timeOfDriverArrival: $("#timeOfDriverArrival").val()
+        }
+        $.ajax({
+            method: 'POST',
+            url: 'api/driver/assign',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(JSONdata),
+            dataType: 'text',
+            success: function (data, textStatus, jqXHR) {
+                alert(data);
+                document.location.href = "driver.jsp";
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                alert(jqXHR.responseText);
+                $("#assignButton").prop('disabled', false);
+            }
+        })
+    });
+
 });
+
+var obj;
 
 function getOrderById(id) {
     $.get("api/order/viewQueuedOrder?id=" + id, function (data) {
-        var obj = JSON.parse(data);
+        obj = JSON.parse(data);
         $("#contactName").attr("value", obj.contactName);
         $("#contactPhone").attr("value", obj.contactPhone);
         $("#requestedSeatsCount").attr("value", obj.requestedSeatsCount);
@@ -24,22 +54,23 @@ function getOrderById(id) {
         $("#distance0").attr("value", obj.distance[0]);
 
         $("#timeRequested").attr("value", obj.timeRequested);
+        $("#timeOfDriverArrival").attr("value", obj.timeRequested);
 
         $('input:radio[name=sex][data-value=' + obj.sex + ']').prop('checked', true);
 
         $("#car").attr("value", obj.carClass);
         $("#music").attr("value", obj.musicType);
 
-        if(obj.smokingFriendly == "true") {
+        if (obj.smokingFriendly == "true") {
             $('input:checkbox[id=smokingFriendly]').prop('checked', true);
         }
-        if(obj.animalFriendly == "true") {
+        if (obj.animalFriendly == "true") {
             $('input:checkbox[id=animalFriendly]').prop('checked', true);
         }
-        if(obj.wifi == "true") {
+        if (obj.wifi == "true") {
             $('input:checkbox[id=wifi]').prop('checked', true);
         }
-        if(obj.airConditioner == "true") {
+        if (obj.airConditioner == "true") {
             $('input:checkbox[id=airConditioner]').prop('checked', true);
         }
 
@@ -52,10 +83,17 @@ function getOrderById(id) {
     });
 }
 
+
 function getUrlVars() {
     var vars = {};
     var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
     });
     return vars;
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
 }
