aa6318ca7dee433a74738c185fa989176243d20e
Alexandelphi <alexandelphi@gmail.com>
2015-05-27 00:59:05 +0300

    rewrote queued order

--- moduleWEB/web/js/view-queued-order.js
+++ moduleWEB/web/js/view-queued-order.js
@@ -6,8 +6,10 @@ $(document).ready(function () {
         $("#timeOfDriverArrival").mask("99/99/9999 99:99", {placeholder: "dd/mm/yyyy hh:mm"});
     });
     getUrlVars();
-    getOrderById(getUrlVars()["id"]);
-    ymaps.ready(init);
+    ymaps.ready(function(){
+        init();
+        getOrderById(getUrlVars()["id"]);
+    });
 
     $("#assignButton").click(function () {
         $("#assignButton").prop('disabled', true);
@@ -39,7 +41,7 @@ var obj;
 
 function getOrderById(id) {
     $.get("api/order/viewQueuedOrder?id=" + id, function (data) {
-        obj = JSON.parse(data);
+        var obj = JSON.parse(data);
         $("#contactName").attr("value", obj.contactName);
         $("#contactPhone").attr("value", obj.contactPhone);
         $("#requestedSeatsCount").attr("value", obj.requestedSeatsCount);
@@ -47,11 +49,47 @@ function getOrderById(id) {
         $("#fromX").attr("value", obj.fromX);
         $("#fromY").attr("value", obj.fromY);
 
-        //TODO add dynamic generation
-        $("#toAddress0").attr("value", obj.toAddress[0]);
-        $("#toX0").attr("value", obj.toX[0]);
-        $("#toY0").attr("value", obj.toY[0]);
-        $("#distance0").attr("value", obj.distance[0]);
+        for(var i = 0; i < obj.toAddress.length; i++){
+            var outer = document.getElementById("importantInfo");
+            var input = document.createElement("input");
+
+            input.setAttribute("type", "text");
+            input.setAttribute("id", "toAddress" + i);
+            input.setAttribute("onchange", "clearToXY(this)");
+            input.setAttribute("placeholder", "To address " + i);
+            input.setAttribute("disabled", "disabled");
+            input.setAttribute("value", obj.toAddress[i]);
+
+            var input2 = document.createElement("input");
+            input2.setAttribute("hidden", "hidden");
+            input2.setAttribute("type", "text");
+            input2.setAttribute("id", "toX" + i);
+            input2.setAttribute("value", obj.toX[i]);
+
+            var input3 = document.createElement("input");
+            input3.setAttribute("hidden", "hidden");
+            input3.setAttribute("type", "text");
+            input3.setAttribute("id", "toY" + i);
+            input3.setAttribute("value", obj.toY[i]);
+
+            var input4 = document.createElement("input");
+            input4.setAttribute("disabled", "disabled");
+            input4.setAttribute("type", "text");
+            input4.setAttribute("id", "distance" + i);
+            input4.setAttribute("value", obj.distance[i]);
+
+            var button = document.getElementById("addPathPoint");
+
+            outer.insertBefore(input, button);
+            outer.insertBefore(input2, button);
+            outer.insertBefore(input3, button);
+            outer.insertBefore(input4, button);
+
+            makeSearch(input);
+            $("#toAddress" + i).change(function () {
+                buildPath($("[id^='toAddress']").length - 1);
+            });
+        }
 
         $("#timeRequested").attr("value", obj.timeRequested);
         $("#timeOfDriverArrival").attr("value", obj.timeRequested);
@@ -61,20 +99,19 @@ function getOrderById(id) {
         $("#car").attr("value", obj.carClass);
         $("#music").attr("value", obj.musicType);
 
-        if (obj.smokingFriendly == "true") {
+        if(obj.smokingFriendly == "true") {
             $('input:checkbox[id=smokingFriendly]').prop('checked', true);
         }
-        if (obj.animalFriendly == "true") {
+        if(obj.animalFriendly == "true") {
             $('input:checkbox[id=animalFriendly]').prop('checked', true);
         }
-        if (obj.wifi == "true") {
+        if(obj.wifi == "true") {
             $('input:checkbox[id=wifi]').prop('checked', true);
         }
-        if (obj.airConditioner == "true") {
+        if(obj.airConditioner == "true") {
             $('input:checkbox[id=airConditioner]').prop('checked', true);
         }
 
-        //TODO this value is empty
         $("#totalMultiplier").attr("value", obj.totalMultiplier);
         $("#totalLength").attr("value", obj.totalLength);
         $("#totalPrice").attr("value", obj.totalPrice);
